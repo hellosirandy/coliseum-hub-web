4764cc095f7dec244f3cd33cab952050bc4c21a9
hellosirandy <sac840711@gmail.com>
2018-06-27 00:03:43 +0800

    change color theme XD

--- src/components/AddStadiumModal/DropDown.js
+++ src/components/AddStadiumModal/DropDown.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import ListItemIcon from '@material-ui/core/ListItemIcon/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ImageIcon from '@material-ui/icons/Image';
+import InsertLinkIcon from '@material-ui/icons/InsertLink';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
@@ -26,8 +28,19 @@ export const DropDown = ({ anchorEl, onClose, onURLClick }) => {
       open={Boolean(anchorEl)}
       onClose={onClose}
     >
-      <MenuItem onClick={() => { onURLClick(); onClose(); }}>URL</MenuItem>
-      <MenuItem>File</MenuItem>
+      <MenuItem onClick={() => { onURLClick(); onClose(); }}>
+        <ListItemIcon>
+          <InsertLinkIcon />
+        </ListItemIcon>
+        <ListItemText inset primary="URL" />
+      </MenuItem>
+      <MenuItem>
+        <ListItemIcon>
+          <ImageIcon />
+        </ListItemIcon>
+        <ListItemText inset primary="Select" />
+      </MenuItem>
+
     </Menu>
   );
 };
@@ -38,7 +51,9 @@ export const InputDialog = ({ open, handleClose }) => {
       open={open}
       onClose={handleClose}
       aria-labelledby="form-dialog-title"
+      fullWidth
     >
+      <DialogTitle id="form-dialog-title">Input URL</DialogTitle>
       <DialogContent>
         <TextField
           margin="dense"
